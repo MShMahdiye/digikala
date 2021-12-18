4274a71d70a0dfe462392e306269ebf7e0bbcf44
MShMahdiye <mshcomen8013@gmail.com>
2021-12-18 11:29:34 +0330

    digi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import products from "./Product";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      products: [],
+      inputvalue: ''
+
+    }
+  }
+  componentDidMount() {
+    this.setState({ products })
+  }
+
+  searchProduct = (value) => {
+    this.setState({ inputvalue: value })
+  }
+
+  clickHandler = () => {
+    console.clear()
+
+    // const url = `https://www.digikala.com/search/?q=${this.state.inputvalue}`
+    const url = `https://www.digikala.com/ajax/autosuggest/?q=${this.state.inputvalue}`
+
+    fetch(url).then(
+      res => res.json()
+    ).then(
+      data => {
+        const list = data.data.suggestion_products.map(item => {
+          return {
+            title: item.title,
+            price: '$20000',
+            imageURL: item.image
+          }
+        })
+        this.setState({ products: list })
+      }
+    )
+
+
+    //   fetch(url).then(
+    //     res => { return res.text() }
+    //   ).then(
+    //     data => {
+    //       const doc = document.createElement('html');
+    //       console.log(data.includes('c-product-box'))
+    //       doc.innerHTML = data;
+    //       const ProductArray = [...doc.querySelectorAll('.c-product-box')]
+    //         .filter(item => !!item && !!item.getAttribute('data-enhanced-ecommerce'));
+    //       const list = ProductArray.map(htmlElement => {
+    //         const ecommerceData = JSON.parse(htmlElement.getAttribute('data-enhanced-ecommerce'));
+    //         const imgElement = htmlElement.querySelector('img');
+    //         const imageURL = imgElement.getAttribute('src')
+    //         return {
+    //           title: ecommerceData.name,
+    //           price: ecommerceData.price,
+    //           imageURL
+    //         }
+    //       })
+    //       this.setState({ products: list })
+    //     }
+    //   )
+
+  }
+ 
+  render() {
+
+    return (
+      <>
+        <div className="head">
+        <div className="circle maincircle"></div>
+        <span className="circle2 minicircle"></span>
+          <div className="header">
+            <h3 className="catalogheader">Product catalog</h3>
+          </div>
+          <div className="search">
+            <label htmlFor="search">search:</label>
+            <input type="text" id="search" name="search" placeholder="search here..." value={this.state.inputvalue}
+              onChange={(event) => { this.setState({ inputvalue: event.target.value }) }} />
+            <button onClick={this.clickHandler} className="search-btn"><FontAwesomeIcon icon={faSearch} /></button>
+            <br></br>
+          </div>
+        </div>
+        <div className="main">
+          {
+            this.state.products.map((item, i) => {
+              console.log('item: ', item);
+              return (
+                <div className="big">
+                  <div className="container">
+                    <div className="image">
+                      <img className="img" src={item.imageURL} width={200} height={200} />
+                    </div>
+                    <div className="info">
+                      <h5 className="title">{item.title}</h5>
+                      <h4 className="price">{item.price}</h4>
+                    </div>
+                    <div>
+                      <span class="foot"><span className="iconbag"><FontAwesomeIcon icon={faShoppingBag} /></span>BUY</span>
+                      <span class="foot mr-50"><span className="iconcart"><FontAwesomeIcon icon={faShoppingCart} /></span>ADD TO CARD</span>
+                    </div>
+                  </div>
+                </div>
+
+              )
+            })
+          }
+        </div>
+        <svg viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="2" d="M0,160L30,170.7C60,181,120,203,180,224C240,245,300,267,360,266.7C420,267,480,245,540,250.7C600,256,660,288,720,293.3C780,299,840,277,900,256C960,235,1020,213,1080,208C1140,203,1200,213,1260,224C1320,235,1380,245,1410,250.7L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+      </>
+
+    )
+
+  }
 }
-
 export default App;
---
+++ src/Product.js
@@ -0,0 +1,32 @@
+export default[
+    {
+        title: "Philips 55put6004 Smart LED TV 55 Inch",
+        price: "15,150,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/3b4ff9c0c4ea78c5bef6b77e6886b593af0306df_1630756373.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    },
+    {
+        title: "Gplus GTV-65KE821S Smart LED TV 65Inch",
+        price: "22,940,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/643433be788c0754f868efdd10683f0aa189da6d_1600862588.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    },
+    {
+        title: "Snowa SLD-50SA1260U LED TV 50 Inch",
+        price: "10,850,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/d63d6ca2e3cdde7d04703fed68199b2e6c348fe5_1627281062.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    },
+    {
+        title: "Snowa SLD-32SA1220 LED TV 32 Inch",
+        price: "4,995,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/be36ece1b4da9ca38915e7d130e964568f0e5ccd_1606139284.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    },
+    {
+        title: "Daewoo DLE-32H1810 LED TV 32 Inch",
+        price: "14,150,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/f94af37ec80eb0bdf37257fd861bef1a8f29a6c1_1606288237.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    },
+    {
+        title: "SAM ELECTRONIC SMART LED TV 58TU6500 58 INCH ULTRA HD",
+        price: "14,750,000 t",
+        imageURL: "https://dkstatics-public.digikala.com/digikala-products/fbd6cfa3198619a528fc77e778b8b625b9afc9c7_1634978109.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80"
+    }
+]
